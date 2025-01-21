af8b68a48685f1de33c52c1aadaeb43a91c8ae21
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-01-21 20:51:46 +0100

    Refactor pointer tests

--- test/pointer.test.ts
+++ test/pointer.test.ts
@@ -1,13 +1,19 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { createStruct } from '../src/struct/createStruct';
 
 describe('Pointer', () => {
-  it('should return pointer', () => {
-    const buffer = new ArrayBuffer(4);
-    const view = new DataView(buffer);
-    view.setUint32(0, 1234, true);
+  let buffer: ArrayBuffer;
+  let view: DataView;
+  let struct: ReturnType<typeof createStruct>; // Struct<ParsedData>
 
-    const struct = createStruct();
+  beforeEach(() => {
+    buffer = new ArrayBuffer(8);
+    view = new DataView(buffer);
+    struct = createStruct();
+  });
+
+  it('should return pointer', () => {
+    view.setUint32(0, 1234, true);
     struct.addMember('pointer').pointer();
 
     const result = struct.parse(view, 0);
@@ -15,11 +21,7 @@ describe('Pointer', () => {
   });
 
   it('should return 0 for allowed null pointer', () => {
-    const buffer = new ArrayBuffer(4);
-    const view = new DataView(buffer);
     view.setUint32(0, 0, true);
-
-    const struct = createStruct();
     struct.addMember('pointer').pointer({ allowNullPointer: true });
 
     const result = struct.parse(view, 0);
@@ -27,41 +29,30 @@ describe('Pointer', () => {
   });
 
   it('should return null for not allowed null pointer', () => {
-    const buffer = new ArrayBuffer(4);
-    const view = new DataView(buffer);
     view.setUint32(0, 0, true);
-
-    const struct = createStruct();
     struct.addMember('pointer').pointer({ allowNullPointer: false });
 
     const result = struct.parse(view, 0);
     expect(result.pointer).toBeNull();
   });
 
+  it('should return pointer at offset', () => {
+    view.setUint32(4, 1234, true);
+    struct.addMember('pointer').pointer();
+
+    const result = struct.parse(view, 4);
+    expect(result.pointer).toBe(1234);
+  });
+
   it('should return multiple defined pointers', () => {
-    const buffer = new ArrayBuffer(8);
-    const view = new DataView(buffer);
     view.setUint32(0, 1234, true);
     view.setUint32(4, 5678, true);
 
-    const struct = createStruct();
     struct.addMember('pointer1').pointer();
     struct.addMember('pointer2').pointer();
 
     const result = struct.parse(view, 0);
     expect(result.pointer1).toBe(1234);
     expect(result.pointer2).toBe(5678);
-  });
-
-  it('should return pointer at offset', () => {
-    const buffer = new ArrayBuffer(8);
-    const view = new DataView(buffer);
-    view.setUint32(4, 1234, true);
-
-    const struct = createStruct();
-    struct.addMember('pointer').pointer();
-
-    const result = struct.parse(view, 4);
-    expect(result.pointer).toBe(1234);
   });
 });
